69235d33b8a906cec156a7a48a6c78486f525c3b
Vipul Chodankar <vipul72c@gmail.com>
2019-10-01 21:51:04 +0530

    Added partials

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the movie search app!");
+  res.render("search");
 });
 
 app.get("/results", (req, res) => {
