a0bd57cbefc1018bbbb2e668bce1992f9e6e79fa
Vipul Chodankar <vipul72c@gmail.com>
2019-10-01 21:40:51 +0530

    Basic results

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require("express");
 const request = require("request");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Welcome to the movie search app!");
 });
 
 app.get("/results", (req, res) => {
-  res.send("Results Route!");
+  request(
+    "http://www.omdbapi.com/?s=avengers&apikey=8623c884",
+    (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        res.render("results", {
+          data: data
+        });
+      }
+    }
+  );
 });
 
 const PORT = process.env.PORT || 6969;
