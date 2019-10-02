1b7fb0471f1882b072ffd378416b93a033635f65
Vipul Chodankar <vipul72c@gmail.com>
2019-10-02 12:08:24 +0530

    Search Functionality

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("search");
 });
 
 app.get("/results", (req, res) => {
-  request(
-    "http://www.omdbapi.com/?s=avengers&apikey=8623c884",
-    (err, response, body) => {
-      if (!err && response.statusCode === 200) {
-        const data = JSON.parse(body);
-        res.render("results", {
-          data: data
-        });
-      }
+  var query = req.query.search;
+  var url = "http://www.omdbapi.com/?s=" + query + "&apikey=8623c884";
+  request(url, (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      res.render("results", {
+        data: data
+      });
     }
-  );
+  });
 });
 
 const PORT = process.env.PORT || 6969;
